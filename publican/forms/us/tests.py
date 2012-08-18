794f39e275214be7052ca7bab6b2154f43f07c1f
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-18 11:40:35 -0400

    Renamed the 940, 941 modules to start with "form_"

--- publican/forms/us/tests.py
+++ publican/forms/us/tests.py
@@ -4,19 +4,19 @@ from unittest import TestCase
 from publican.engine.kit import Quarter, Year
 from publican.engine.tests.sample import company
 from .. common import Filing
-from . import irs_940, irs_941
+from . import form_940, form_941
 
 
 class Test940(TestCase):
 
     def test_periods(self):
         self.assertEqual(
-            [ unicode(period) for period in irs_940.periods(company) ],
+            [ unicode(period) for period in form_940.periods(company) ],
             [u'2011', u'2012']
             )
 
     def test_reckon(self):
-        f = Filing(irs_940, Year(2012))
+        f = Filing(form_940, Year(2012))
         f.tally(company)
         p = f.pages[0]
         self.assertEqual(p.line3, Decimal('9700.00'))
@@ -28,12 +28,12 @@ class Test941(TestCase):
 
     def test_periods(self):
         self.assertEqual(
-            [ unicode(period) for period in irs_941.periods(company) ],
+            [ unicode(period) for period in form_941.periods(company) ],
             [u'2011-Q3', u'2011-Q4', u'2012-Q1', u'2012-Q2', u'2012-Q3']
             )
 
     def test_reckon(self):
-        f = Filing(irs_941, Quarter(2012, 1))
+        f = Filing(form_941, Quarter(2012, 1))
         f.tally(company)
         p = f.pages[0]
         self.assertEqual(p.line2, Decimal('7500.00'))
