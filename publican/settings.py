4cb309225422df913403efe1862397e7b6587113
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-17 20:15:26 -0400

    Added an empty "api" and "engine" application

--- publican/settings.py
+++ publican/settings.py
@@ -119,6 +119,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'publican.api',
+    'publican.engine',
     'publican.frontend',
 )
 
