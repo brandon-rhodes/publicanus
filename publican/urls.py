2885c68b856e5936587c25d8dae59dea3fef8970
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-17 20:10:56 -0400

    Added a "frontend" app and turned on "staticfiles"

--- publican/urls.py
+++ publican/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'publican.views.home', name='home'),
     # url(r'^publican/', include('publican.foo.urls')),
@@ -14,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+    )
+
+urlpatterns += staticfiles_urlpatterns()
