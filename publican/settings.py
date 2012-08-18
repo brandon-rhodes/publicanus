0d22822b53b11d4a5b72b9063a0486829503d5ac
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-18 11:01:53 -0400

    Discovered the wonderful Django USE_THOUSAND_SEPARATOR

--- publican/settings.py
+++ publican/settings.py
@@ -49,6 +49,8 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+USE_THOUSAND_SEPARATOR = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
