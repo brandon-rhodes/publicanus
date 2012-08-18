0e2532808e4c8199ead7764b1c8f959906bdb6b1
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-18 08:31:06 -0400

    Vastly simplified main view with a "Row" class

--- publican/frontend/views.py
+++ publican/frontend/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime
 from itertools import groupby
 
@@ -8,27 +9,33 @@ from publican.engine.tests.sample import company
 from publican.forms.common import Filing
 from publican.forms.registry import all_forms
 
+class Row(object):
+    """Throwaway class for the table rows we build for the template."""
+
 def _display_month(filing):
     d = filing.due_date
     return datetime(d.year, d.month + (1 if d.day < 6 else 0), 1)
 
 def index(request):
 
-    seq = []
-    for form in all_forms():
+    months = defaultdict(list)
+
+    for form in sorted(all_forms(), key=lambda f: f.name):
         for period in form.periods(company):
             filing = Filing(form, period)
             filing.tally(company)
-            filing.display_month = _display_month(filing)
-            seq.append(filing)
+            display_month = _display_month(filing)
+            months[display_month].append(filing)
 
-    seq.sort(key=lambda f: f.form.name)
-    seq.sort(key=lambda f: f.display_month)
+    rows = []
 
-    seq = [[k, list(v)] for k, v in groupby(seq, lambda f: f.display_month)]
-    for item in seq:
-        item.append(cents(sum(t.balance_due for t in item[1])))
+    for month, filings in sorted(months.iteritems()):
+        row = Row()
+        row.month = month
+        row.filings = filings
+        row.total_due = cents(sum(f.balance_due for f in filings))
+        rows.append(row)
 
     return render_to_response('publican/main.html', {
-            'seq': seq
+            'rows': rows,
             })
