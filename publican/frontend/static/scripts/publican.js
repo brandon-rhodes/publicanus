4a27d0076578d519b42b2f9b02dc3d499be2ea83
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-19 01:00:32 -0400

    Stubbed out a click() routine for filing deletion

--- publican/frontend/static/scripts/publican.js
+++ publican/frontend/static/scripts/publican.js
@@ -1,5 +1,10 @@
 /* Publican JavaScript. */
 
 $(document).ready(function() {
-    ;
+
+    $('.delete-filing').click(function(event) {
+        var url = $(event.delegateTarget).attr('data-url');
+        console.log('We want to delete the filing at', url);
+    });
+
 });
