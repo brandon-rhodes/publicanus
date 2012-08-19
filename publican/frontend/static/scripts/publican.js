c0bfa9e5574ebb2195cbbd95fafce4d79b436b71
Brandon Rhodes <brandon@rhodesmill.org>
2012-08-19 13:59:18 -0400

    Cleaned up the JS routine with no ill effect

--- publican/frontend/static/scripts/publican.js
+++ publican/frontend/static/scripts/publican.js
@@ -11,49 +11,49 @@ $(document).ready(function() {
         $.ajax({url: url, type: 'DELETE', success: reload_page});
     });
 
-    var create_input = null;
+    var $create_button = $('.create-filing');
+    var $create_input = null;
 
-    $('.create-filing').click(function(event) {
-        if (create_input !== null) {
-            create_input.show();
-            create_input.focus();
+    $create_button.click(function(event) {
+        if ($create_input !== null) {
+            $create_input.show();
+            $create_input.focus();
             return;
         }
 
         var go = function(e) {
-            var val = create_input.val();
+            var val = $create_input.val();
             if (!/^[0-9]+\/[0-9]+\/[0-9]+/.test(val))
                 return;
+
             var url = $('.create-filing').attr('data-url');
             var json = $('.create-filing').attr('data-json');
+
             var data = $.parseJSON(json);
-            console.log(data);
             data.date = val;
-            console.log(data);
+
             $.ajax({
-                url: 'http://localhost:8000/api/v1/filing/',
+                url: url,
                 type: 'POST',
                 contentType: 'application/json',
                 data: JSON.stringify(data),
-                dataType: 'json',
-                processData: false,
                 success: function() {
                     reload_page();
                 }
             });
-            create_input.hide();
+            $create_input.hide();
         };
 
-        create_input = $('<input>');
-        $('.create-filing').parent().after(create_input);
-        create_input.datepicker({
+        $create_input = $('<input>');
+        $create_button.parent().after($create_input);
+        $create_input.datepicker({
             minDate: '1/1/1990', maxDate: '+1d', onSelect: go,
             appendText: '<br>When did you file?'
         });
-        create_input.keypress(function(e) {
+        $create_input.keypress(function(e) {
             if (e.which === 13)  // enter key
                 go();
         });
-        create_input.focus();
+        $create_input.focus();
     });
 });
